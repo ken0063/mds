0ee4e40a62db527fef88d73a9c402089a5c69355
ken0063 <olukotunk@gmail.com>
2022-09-30 13:39:21 +0100

    fix(ci): fix CI build step
    Remove organism from the script in the scss directory

--- packages/scss/src/scripts/build.js
+++ packages/scss/src/scripts/build.js
@@ -4,7 +4,7 @@ const Sass = require('node-sass');
 
 const getComponents = () => {
   let allComponents = [];
-  const types = ['atoms', 'molecules', 'organisms'];
+  const types = ['atoms', 'molecules'];
 
   types.forEach((type) => {
     const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
